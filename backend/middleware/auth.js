40a2a0a76706948a4638d4066de60513533b136b
hopeingoderi <merry.grace44@yahoo.com>
2026-02-04 20:30:29 +0100

    fix Certificate error

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,8 @@ function requireAuth(req, res, next) {
   if (!req.session?.user) {
     return res.status(401).json({ error: "Not logged in" });
   }
-  next();
+  req.user = req.session.user;
+    next();
 }
 
 function requireAdmin(req, res, next) {
